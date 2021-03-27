57a1b13fbfa068f56913dc1b7aebfd44cb38feeb
Jan Majewski <jan.majewski@student.put.poznan.pl>
2021-03-27 11:25:47 +0100

    Add project files.

---
+++ Komunikator_TIP/Program.cs
@@ -0,0 +1,79 @@
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Komunikator_TIP
+{
+    class Program
+    {
+        static WaveInEvent s_WaveIn;
+        static WaveOut wo;
+        static void Main(string[] args)
+        {
+            getDevices();
+            getDevicesOut();
+            using (var ms = File.OpenRead("test.mp3"))
+            using (var rdr = new Mp3FileReader(ms))
+            using (var wavStream = WaveFormatConversionStream.CreatePcmStream(rdr))
+            using (var baStream = new BlockAlignReductionStream(wavStream))
+            using (var waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+            {
+                waveOut.Init(baStream);
+                waveOut.Play();
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            //init();
+            //while (true) /* Yeah, this is bad, but just for testing.... */
+            //    System.Threading.Thread.Sleep(3000);
+        }
+
+        public static void init()
+        {
+            s_WaveIn = new WaveInEvent();
+            s_WaveIn.WaveFormat = new WaveFormat(44100, 2);
+            s_WaveIn.DeviceNumber = 0;
+            s_WaveIn.BufferMilliseconds = 1000;
+            s_WaveIn.DataAvailable += new EventHandler<WaveInEventArgs>(SendCaptureSamples);
+            s_WaveIn.StartRecording();
+        }
+
+        static void SendCaptureSamples(object sender, WaveInEventArgs e)
+        {
+            Console.WriteLine("Bytes recorded: {0}", e.BytesRecorded);
+            byte[] bytes = new byte[1024];
+
+            IWaveProvider provider = new RawSourceWaveStream(
+                                     new MemoryStream(bytes), new WaveFormat());
+
+            wo.Init(provider);
+            wo.Play();
+
+        }
+
+        public static void getDevices()
+        {
+            int waveInDevices = WaveIn.DeviceCount;
+            for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
+            {
+                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(waveInDevice);
+                Console.WriteLine("Device {0}: {1}, {2} channels", waveInDevice, deviceInfo.ProductName, deviceInfo.Channels);
+            }
+        }
+        public static void getDevicesOut()
+        {
+            int waveInDevices = WaveOut.DeviceCount;
+            for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
+            {
+                WaveOutCapabilities deviceInfo = WaveOut.GetCapabilities(waveInDevice);
+                Console.WriteLine("Device {0}: {1}, {2} channels", waveInDevice, deviceInfo.ProductName, deviceInfo.Channels);
+            }
+        }
+
+
+    }
+}
---
+++ TIP_Client/ViewModel/MainVM.cs
@@ -0,0 +1,149 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TIP_Client.ViewModel.MVVM;
+
+namespace TIP_Client.ViewModel
+{
+    public class MainVM : ViewModelBase
+    {
+        private WaveInEvent waveIn;
+        public MainVM()
+        {
+            OutputDeviceList = new ObservableCollection<WaveOutCapabilities>();
+            int waveOutDevices = WaveOut.DeviceCount;
+            for (int waveOutDevice = 0; waveOutDevice < waveOutDevices; waveOutDevice++)
+            {
+                OutputDeviceList.Add(WaveOut.GetCapabilities(waveOutDevice));
+            }
+
+            InputDeviceList = new ObservableCollection<WaveInCapabilities>();
+            int waveInDevices = WaveIn.DeviceCount;
+            for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
+            {
+                InputDeviceList.Add(WaveIn.GetCapabilities(waveInDevice));
+            }
+
+
+
+            RecordCommand = new Command((args) =>
+            {
+                init();
+            });
+        }
+        private ViewModelBase selectedVM;
+        public ViewModelBase SelectedVM
+        {
+            get
+            {
+                return selectedVM;
+            }
+            set
+            {
+                selectedVM = value;
+                OnPropertyChanged(nameof(SelectedVM));
+            }
+        }
+        public ICommand RecordCommand { get; set; }
+
+        private ObservableCollection<WaveOutCapabilities> outputDeviceList;
+        public ObservableCollection<WaveOutCapabilities> OutputDeviceList
+        {
+            get
+            {
+                return outputDeviceList;
+            }
+            set
+            {
+                outputDeviceList = value;
+                OnPropertyChanged(nameof(OutputDeviceList));
+            }
+        }
+        private WaveOutCapabilities outputDeviceSelected;
+        public WaveOutCapabilities OutputDeviceSelected
+        {
+            get
+            {
+                return outputDeviceSelected;
+            }
+            set
+            {
+                outputDeviceSelected = value;
+                OnPropertyChanged(nameof(OutputDeviceSelected));
+            }
+        }
+
+        private ObservableCollection<WaveInCapabilities> inputDeviceList;
+        public ObservableCollection<WaveInCapabilities> InputDeviceList
+        {
+            get
+            {
+                return inputDeviceList;
+            }
+            set
+            {
+                inputDeviceList = value;
+                OnPropertyChanged(nameof(InputDeviceList));
+            }
+        }
+        private WaveInCapabilities inputDeviceSelected;
+        public WaveInCapabilities InputDeviceSelected
+        {
+            get
+            {
+                return inputDeviceSelected;
+            }
+            set
+            {
+                inputDeviceSelected = value;
+                OnPropertyChanged(nameof(InputDeviceSelected));
+            }
+        }
+        private bool recordChecked;
+        public bool RecordChecked
+        {
+            get
+            {
+                return recordChecked;
+            }
+            set
+            {
+                recordChecked = value;
+                OnPropertyChanged(nameof(RecordChecked));
+            }
+        }
+
+
+        private void init()
+        {
+            waveIn = new WaveInEvent();
+            waveIn.WaveFormat = new WaveFormat(44100, 2);
+            waveIn.DeviceNumber = getDevice(InputDeviceSelected);
+            waveIn.BufferMilliseconds = 1000;
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(SendCaptureSamples);
+            waveIn.StartRecording();
+        }
+
+        static void SendCaptureSamples(object sender, WaveInEventArgs e)
+        {
+            Console.WriteLine("Bytes recorded: {0}", e.BytesRecorded);
+            byte[] bytes = new byte[1024];
+
+        }
+        private int getDevice(WaveInCapabilities wIn)
+        {
+            int waveInDevices = WaveIn.DeviceCount;
+            for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
+            {
+                if (wIn.ProductName == WaveIn.GetCapabilities(waveInDevice).ProductName) return waveInDevice;
+            }
+            return 0;
+        }
+    }
+}
