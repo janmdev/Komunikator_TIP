70374c2b22379117d5d6c323b212d83e82f7192c
Marcin Tajsner <marcin.tajsner@gmail.com>
2021-06-28 22:54:22 +0200

    small fix

--- Shared/TCP_Protocol.cs
+++ Shared/TCP_Protocol.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,24 +19,33 @@ namespace Shared
         private const int dataSizeBufferSize = 2;
         private const int dataBufferSize = 1024;
 
-        public static void Send(NetworkStream stream, byte code, string dataJSON) {
+        public static void Send(NetworkStream stream, byte code, string dataJSON)
+        {
             byte[] codeBuffer = new byte[codeBufferSize];
             byte[] dataSizeBuffer = new byte[dataSizeBufferSize];
             byte[] dataJSONBytes = Encoding.ASCII.GetBytes(dataJSON);
 
-            codeBuffer[0] = code;
-            stream.Write(codeBuffer, 0, codeBufferSize);
+            try
+            {
+                codeBuffer[0] = code;
+                stream.Write(codeBuffer, 0, codeBufferSize);
 
-            ushort dataSize = (ushort)dataJSONBytes.Length;
-            dataSizeBuffer[0] = (byte)dataSize;
-            dataSizeBuffer[1] = (byte)(dataSize >> 8);
-            stream.Write(dataSizeBuffer, 0, dataSizeBufferSize);
+                ushort dataSize = (ushort)dataJSONBytes.Length;
+                dataSizeBuffer[0] = (byte)dataSize;
+                dataSizeBuffer[1] = (byte)(dataSize >> 8);
+                stream.Write(dataSizeBuffer, 0, dataSizeBufferSize);
 
-            stream.Write(dataJSONBytes, 0, dataSize);
-            stream.Flush();
+                stream.Write(dataJSONBytes, 0, dataSize);
+                stream.Flush();
+            }
+            catch (IOException)
+            {
+                return;
+            }
         }
 
-        public static (byte code, string dataJSON) Read(NetworkStream stream) {
+        public static (byte code, string dataJSON) Read(NetworkStream stream)
+        {
             byte[] codeBuffer = new byte[dataSizeBufferSize];
             byte[] dataSizeBuffer = new byte[dataSizeBufferSize];
             byte[] dataBuffer = new byte[dataBufferSize];
@@ -46,23 +56,31 @@ namespace Shared
             byte code;
             string dataJSON = "";
 
-            stream.Read(codeBuffer, 0, codeBufferSize);
-            stream.Read(dataSizeBuffer, 0, dataSizeBufferSize);
+            try
+            {
+                stream.Read(codeBuffer, 0, codeBufferSize);
+                stream.Read(dataSizeBuffer, 0, dataSizeBufferSize);
 
-            code = codeBuffer[0];
-            dataSize = BitConverter.ToUInt16(dataSizeBuffer, 0);
+                code = codeBuffer[0];
+                dataSize = BitConverter.ToUInt16(dataSizeBuffer, 0);
 
-            int dataBytesLeft = dataSize;
+                int dataBytesLeft = dataSize;
 
-            while (dataBytesLeft > 0) {
-                int dataBytesRead = stream.Read(dataBuffer, 0, dataBufferSize);
-                char[] chars = new char[decoder.GetCharCount(dataBuffer, 0, dataBytesRead)];
-                decoder.GetChars(dataBuffer, 0, dataBytesRead, chars, 0);
-                dataJSON += new string(chars);
-                dataBytesLeft -= dataBytesRead;
+                while (dataBytesLeft > 0)
+                {
+                    int dataBytesRead = stream.Read(dataBuffer, 0, dataBufferSize);
+                    char[] chars = new char[decoder.GetCharCount(dataBuffer, 0, dataBytesRead)];
+                    decoder.GetChars(dataBuffer, 0, dataBytesRead, chars, 0);
+                    dataJSON += new string(chars);
+                    dataBytesLeft -= dataBytesRead;
+                }
+
+                return (code, dataJSON);
             }
-
-            return (code, dataJSON);
+            catch (IOException)
+            {
+                return (0, "");
+            }
         }
 
         public static async Task<(byte code, string dataJSON)> ReadAsync(NetworkStream stream)
